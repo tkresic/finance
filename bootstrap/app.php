78f7fa9563df4fe03d806582e12132755a1b7fb8
Toni Krešić <tonikresic1997@gmail.com>
2021-06-15 21:03:07 +0200

    {app}: [add] - cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -74,9 +74,10 @@ $app->configure('services');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\Cors::class
+]);
+
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
